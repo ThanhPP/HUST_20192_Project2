e0bebc36451eabdb273c21aabc7fb66c9d90906f
Phan Phu Thanh <thanhphanphu18@gmail.com>
2020-04-11 22:10:21 +0700

    Demo program APPL

--- Test042020/main.py
+++ Test042020/main.py
@@ -1,74 +1,56 @@
+import os
 import datetime as dt
-import time
-
-import matplotlib.pyplot as pyplot
-import pandas as pd
-import pandas_datareader as pdr
-import tensorflow as tf
-import yfinance as yf
-import numpy as np
-from sklearn.preprocessing import RobustScaler
 
 from Test042020 import ticker_data as td
+from Test042020 import model as md
 
-# library config
-yf.pdr_override()
-# pd.set_option('display.max_columns', None)
-print(tf.__version__)
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.layers import LSTM
 
-# dataset
-tickers = ["AAPL", "MSFT"]
-start = dt.datetime(2001, 1, 1)
-end = dt.datetime(2019, 12, 31)
+# CONFIG
+# model
+BATCH_SIZE = 100
+EPOCHS = 20
+UNITS = 256
+CELL = LSTM
+N_LAYERS = 2
+DROPOUT = 0.3
+LOSS = "mean_absolute_error"
+OPTIMIZER = "rmsprop"
+# data
+TICKER = "AAPL"
+START = dt.datetime(2001, 1, 1)
+END = dt.datetime(2019, 12, 31)
+FEATURE_COLUMNS = ['Adj Close', 'Volume', 'Open', 'High', 'Low']
+LOOKUP_STEPS = 1
+N_STEPS = 50
+TEST_SIZE = 0.1
 
-# flag
-readfile_flag = False
-
-
-def create_dataset(X, y, steps=1):
-    Xs, ys = [], []
-    for i in range(len(X) - steps):
-        v = X.iloc[i: (i + steps)].to_numpy()
-        Xs.append(v)
-        ys.append(y.iloc[i: i + steps])
-    return np.array(Xs), np.array(ys)
+# make dir to store data
+if not os.path.isdir("results"):
+    os.mkdir("results")
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+if not os.path.isdir("data"):
+    os.mkdir("data")
+model_name = f"{TICKER}-{LOSS}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEPS}-layers-{N_LAYERS}-units-{UNITS}"
 
 
 def main():
-    # GET DATA
-    main_df = pd.DataFrame()
-    ticker_data = td.Ticker_Data(main_df)
+    data = td.load_data(ticker=TICKER, n_steps=N_STEPS, lookup_steps=LOOKUP_STEPS, test_size=TEST_SIZE,
+                        feature_columns=FEATURE_COLUMNS)
+    model = md.create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS, dropout=DROPOUT,
+                            optimizer=OPTIMIZER)
 
-    if not readfile_flag:
-        for ticker in tickers:
-            print("Crawling : " + ticker)
-            time.sleep(1)
+    checkpointer = ModelCheckpoint(os.path.join("results", model_name), save_best_only=True, verbose=1)
+    tensorboard = TensorBoard(log_dir=os.path.join("logs", model_name))
 
-            df = pdr.get_data_yahoo(str(ticker), start=start, end=end)
-            df = df.reset_index(level='Date')
-
-            ticker_data.append_default_column(df, ticker, 'Close')
-            ticker_data.append_default_column(df, ticker, 'Volume')
-
-        ticker_data.main_df.to_csv('csv_files\\stock_data_close_and_volume{0}--{1}.csv'.format(str(start.date()),
-                                                                                               str(end.date())),
-                                   mode="w")
-    else:
-        print("Reading data from file")
-        ticker_data.main_df = pd.read_csv('csv_files\\stock_data_close_and_volume{0}--{1}.csv'.format(str(start.date()),
-                                                                                                      str(end.date())),
-                                          index_col=0)
-
-    print(ticker_data.main_df.head())
-
-    # PREPARE THE DATA SET
-    # split train and test set 0.9/0.1
-    train_size = int(len(ticker_data.main_df) * 0.9)
-    test_size = int(len(ticker_data.main_df)) - train_size
-    train = ticker_data.main_df.iloc[0:train_size]
-    test = ticker_data.main_df.iloc[train_size:len(ticker_data.main_df)]
-    print(train.shape, test.shape)
-
+    history = model.fit(data['X_train'], data['y_train'],
+                        batch_size=BATCH_SIZE, epochs=EPOCHS,
+                        validation_data=(data['X_test'], data['y_test']),
+                        callbacks=[checkpointer, tensorboard],
+                        verbose=1)
+    model.save(os.path.join("results", model_name) + ".h5")
 
 
 main()
