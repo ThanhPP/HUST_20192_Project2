968381d9e9a1a85a9fa22674ee84ff6c94b4917d
Phan Phu Thanh <thanhphanphu18@gmail.com>
2020-04-11 17:26:19 +0700

    get data and split data set

--- Test042020/main.py
+++ Test042020/main.py
@@ -1,11 +1,15 @@
-import tensorflow as tf
 import datetime as dt
 import time
-import pandas_datareader as pdr
+
+import matplotlib.pyplot as pyplot
 import pandas as pd
+import pandas_datareader as pdr
+import tensorflow as tf
 import yfinance as yf
-from Test042020 import ticker_data as td
 import numpy as np
+from sklearn.preprocessing import RobustScaler
+
+from Test042020 import ticker_data as td
 
 # library config
 yf.pdr_override()
@@ -13,16 +17,25 @@ yf.pdr_override()
 print(tf.__version__)
 
 # dataset
-tickers = ["AAPL", "MSFT", "GOOG", "FB"]
-start = dt.datetime(2013, 10, 1)
-end = dt.datetime(2017, 4, 14)
+tickers = ["AAPL", "MSFT"]
+start = dt.datetime(2001, 1, 1)
+end = dt.datetime(2019, 12, 31)
 
 # flag
-readfile_flag = True
+readfile_flag = False
+
+
+def create_dataset(X, y, steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - steps):
+        v = X.iloc[i: (i + steps)].to_numpy()
+        Xs.append(v)
+        ys.append(y.iloc[i: i + steps])
+    return np.array(Xs), np.array(ys)
 
 
 def main():
-    # get data
+    # GET DATA
     main_df = pd.DataFrame()
     ticker_data = td.Ticker_Data(main_df)
 
@@ -32,7 +45,7 @@ def main():
             time.sleep(1)
 
             df = pdr.get_data_yahoo(str(ticker), start=start, end=end)
-            df = df.reset_index(level="Date")
+            df = df.reset_index(level='Date')
 
             ticker_data.append_default_column(df, ticker, 'Close')
             ticker_data.append_default_column(df, ticker, 'Volume')
@@ -43,9 +56,19 @@ def main():
     else:
         print("Reading data from file")
         ticker_data.main_df = pd.read_csv('csv_files\\stock_data_close_and_volume{0}--{1}.csv'.format(str(start.date()),
-                                                                                                      str(end.date())))
+                                                                                                      str(end.date())),
+                                          index_col=0)
 
-    print(ticker_data.main_df.shape)
+    print(ticker_data.main_df.head())
+
+    # PREPARE THE DATA SET
+    # split train and test set 0.9/0.1
+    train_size = int(len(ticker_data.main_df) * 0.9)
+    test_size = int(len(ticker_data.main_df)) - train_size
+    train = ticker_data.main_df.iloc[0:train_size]
+    test = ticker_data.main_df.iloc[train_size:len(ticker_data.main_df)]
+    print(train.shape, test.shape)
+
 
 
 main()
