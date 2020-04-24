0d595eafd1f916be8d231470d482930366005c33
Phan Phu Thanh <thanhphanphu18@gmail.com>
2020-04-24 23:28:42 +0700

    init main app

--- README.md
+++ README.md
@@ -12,6 +12,27 @@ _ **Sinh viên thực hiện** : Phan Phú Thành.
 
 _ [**Các tài liệu khác**](https://drive.google.com/open?id=1bVA8XYJ_cDb9mNIbi9VGlNTvnni_0VFJ)
 
+## Các chương trình thử nghiệm :
+
+### 1. [TEST 1](https://github.com/ThanhPP/HUST_20192_Project2/tree/master/Test042020).
+
+[Link tham khảo](https://medium.com/@jasonbamford/machine-learning-algorithm-to-predict-stock-direction-d54b7666cc7c)
+
+### 2. [TEST 2](https://github.com/ThanhPP/HUST_20192_Project2/tree/master/Test042020.)
+
+[Link tham khảo](https://www.thepythoncode.com/article/stock-price-prediction-in-python-using-tensorflow-2-and-keras)
+
+## Chương trình chính :
+
+### 1. Chức năng các file :
+
+_ main.py : chương trình chính để thực thi.
+
+_ ticker.py : để lấy và xử lý dữ liệu về chỉ số chứng khoán.
+
+_ model.py : tạo model từ tensorflow.
+
+
 ## Tài liệu tham khảo : 
 
 _ [Machine Learning cơ bản](https://drive.google.com/open?id=0B7ujsutwirjXLXlwcnZUTjVVRXVjd19WNlVmREdac0xFNGIw)
---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import datetime as dt
+import tensorflow as tf
+
+# from Test042020
+import ticker as td
+import model as md
+# from Test042020
+
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.layers import LSTM
+
+# CONFIG
+# model
+BATCH_SIZE = 365
+EPOCHS = 50
+UNITS = 365
+CELL = LSTM
+N_LAYERS = 2
+DROPOUT = 0.3
+LOSS = "mean_absolute_error"
+OPTIMIZER = "rmsprop"
+# data
+TICKER = "AAPL"
+START = dt.datetime(2000, 1, 1)
+END = dt.datetime(2019, 12, 31)
+FEATURE_COLUMNS = ['Adj Close', 'Volume', 'Open', 'High', 'Low']
+LOOKUP_STEPS = 1
+N_STEPS = 20
+TEST_SIZE = 0.1
+# flag
+# options : train, validate, predict
+train_flag = "validate"
+
+# make dir to store data
+if not os.path.isdir("results"):
+    os.mkdir("results")
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+if not os.path.isdir("data"):
+    os.mkdir("data")
+
+model_name = f"{TICKER}-{LOSS}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEPS}-layers-{N_LAYERS}-units-{UNITS}-" \
+             f"batch_size-{BATCH_SIZE}-epochs{EPOCHS}"
+
+
+def main():
+    data = td.load_data(ticker=TICKER, n_steps=N_STEPS, lookup_steps=LOOKUP_STEPS, test_size=TEST_SIZE,
+                        feature_columns=FEATURE_COLUMNS)
+    model = md.create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS, dropout=DROPOUT,
+                            optimizer=OPTIMIZER)
+    # model.summary()
+
+    if train_flag == "train":
+        checkpointer = ModelCheckpoint(os.path.join("results", model_name), save_best_only=True, verbose=1)
+        tensorboard = TensorBoard(log_dir=os.path.join("logs", model_name))
+
+        history = model.fit(data['X_train'], data['y_train'],
+                            batch_size=BATCH_SIZE, epochs=EPOCHS,
+                            validation_data=(data['X_test'], data['y_test']),
+                            callbacks=[checkpointer, tensorboard],
+                            verbose=1)
+        model.save(os.path.join("results", model_name) + ".h5")
+
+    elif train_flag == "validate" :
+        model_path = os.path.join("results", model_name) + ".h5"
+        model.load_weights(model_path)
+        mse, mae = model.evaluate(data["X_test"], data["y_test"])
+        # calculate the mean absolute error (inverse scaling)
+        mean_absolute_error = data["column_scaler"]["Adj Close"].inverse_transform(mae.reshape(1, -1))[0][0]
+        print("Mean Absolute Error:", mean_absolute_error)
+
+    elif train_flag == "predict" :
+        print("predict")
+
+
+main()
