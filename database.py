340a2c03484b23b40b043b4bef63b2231304b822
Phan Phu Thanh <thanhphanphu18@gmail.com>
2020-05-01 21:50:05 +0700

    add get_stock_data func to db

--- database.py
+++ database.py
@@ -1,19 +1,26 @@
 import mysql.connector
 from mysql.connector import Error
-from mysql.connector import errorcode
+
+# CONFIG
+HOST = "127.0.0.1"
+PORT = "3306"
+DATABASE = "stockdata"
+USER = "root"
+PASSWORD = "Thanh.1809PP"
 
 
-def connect(host, database, usr, pwd):
+def connect(host, port, database, usr, pwd):
     print(f"Connecting to {host}")
     try:
         connection = mysql.connector.connect(
             host=host,
+            port=port,
             database=database,
             user=usr,
             password=pwd
         )
         return connection
-    except mysql.connector.Error as error:
+    except Error as error:
         print(f"Failed to create connection to db : {error}")
         return
 
@@ -28,14 +35,29 @@ def close_connection(connection):
     return
 
 
-def get_stock_data(connection):
+def get_stock_data(ticker):
+    connection = connect(
+        host=HOST,
+        port=PORT,
+        database=DATABASE,
+        usr=USER,
+        pwd=PASSWORD
+    )
+
     cursor = connection.cursor()
 
-    mySQL_query = ""
+    mySQL_query = "SELECT *" \
+                  "FROM " + str(ticker)
+
     cursor.execute(mySQL_query)
 
     records = cursor.fetchall()
+
     print(f"Total rows = {cursor.rowcount}")
 
+    close_connection(connection)
+
+
+get_stock_data("GOOG")
 
 
